b1f1aa0b994ee927f61579e0e99fceea5c6774d1
yavuz7141 <yavuz7141@gmail.com>
2021-05-29 22:26:49 -0400

    userdata updated in instance.tf

--- my_train_sch_terraform/web-server/instance.tf
+++ my_train_sch_terraform/web-server/instance.tf
@@ -13,10 +13,11 @@ resource "aws_instance" "web" {
       sudo yum update -y
       sudo amazon-linux-extras install docker -y
       sudo service docker start
+      sudo systemctl start docker
       sudo usermod -a -G docker ec2-user
       sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
       sudo chmod +x /usr/local/bin/docker-compose
-      sudo docker run -d -p 8096:8080 dzhovid/train-schedule:v3
+      sudo docker run -d -p 8096:8080 yavuz7141/train-schedule:v2
     EOF
   tags = {
     Name = "${var.prefix}-web_server"
